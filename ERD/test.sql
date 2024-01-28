eb209f0130f9dbf9f2b7814661c8c06fa21710d9
cjkywe07 <jiwony99@gmail.com>
2024-01-29 04:44:39 +0900

    마이페이지 커뮤니티 진행중

---
+++ ERD/test.sql
@@ -0,0 +1,16 @@
+SELECT *
+FROM market
+WHERE userId = 1;
+
+SELECT *
+FROM food
+WHERE userId = 2;
+
+SELECT *
+FROM food_save  
+WHERE userId = 2;
+
+SELECT *
+FROM favorite_like 
+WHERE userId = 2;
+
